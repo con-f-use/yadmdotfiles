7e92abf3a06af6128956697d827d3e1aa0511466
con-f-use <con-f-use@gmx.net>
2019-09-22 17:04:25 +0200

    Order shorcut help

---
+++ .config/yadmdotfiles/bin/listshortcuts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "In statistical order; most used to least used:
+    Ctrl + r => reverse history search
+    Alt  + . => Repeat last word
+    Ctrl + d => Logout
+
+    Ctrl + a / e         Go to the start / end of the command line
+    Ctrl + u / k         Cut everything left / right of cursor
+    Alt  + b / f         Move curser one word left / right
+    Ctrl + w / Alt + d   Cut word left / right of the cursor
+    Ctrl + y             Paste previously cut contents
+
+    Alt  + t           Swap the two words under the curor (bash)
+    Ctrl + t           Swap the two characters under the cursor
+    Ctrl + x Ctrl + e  Edit long line in editor
+    Ctrl + l           Clear screen
+
+    Ctrl + x         Glob expand a word
+    Alt + Ctrl + e   Shell-expand line
+
+    !$   Last non-command argument
+    !^   First non-command argument
+    !*   All non-command arguments
+    ^foo^bar   Replace foo by bar in last command"
