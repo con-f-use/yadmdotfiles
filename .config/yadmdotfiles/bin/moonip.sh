bf4ce2f461cc201c6550dc21c8a05210038edf01
con-f-use <con-f-use@gmx.net>
2019-10-16 10:17:37 +0200

    Moonip

---
+++ .config/yadmdotfiles/bin/moonip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Print ip of moonsoft to stdout and clipboard.
+
+moonip=$(
+    ssh cudaws ping moon.emea.cuda-inc.com -c 1 2>/dev/null | 
+        grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' )
+
+echo "$moonip"
+echo "$moonip" |
+    xsel --clipboard --input
+
