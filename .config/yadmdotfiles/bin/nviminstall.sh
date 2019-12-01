9fb64436376db31dcd9d9f7ed62d81e22d265773
con-f-use <con-f-use@gmx.net>
2019-12-01 22:08:11 +0100

    fix nvim install whitespaces

--- .config/yadmdotfiles/bin/nviminstall.sh
+++ .config/yadmdotfiles/bin/nviminstall.sh
@@ -3,21 +3,35 @@ description="./$0 -h
 Locally build and install neovim from source without root.
 
 date: Sun Dec  1 21:02:12 CET 2019
-author: confus <con-f-use@gmx.net>" 
+author: confus <con-f-use@gmx.net>"
 
 libfile="$HOME/.config/yadmdotfiles/bash/jcgb.bash"
 [ -r "$libfile" ] &&
     source "$libfile" ||
     { 2>echo "Requires '$libfile'!"; exit 1; }
 
+ubuntu_install_prereqs() {
+    if command -v apt-get &>dev/null; then
+        sudo apt-get install \
+            ninja-build gettext \
+            libtool libtool-bin \
+            autoconf automake cmake \
+            g++ pkg-config unzip
+    fi
+}
+
 main() {
+    ubuntu_install_prereqs
     oldpwd=$(pwd)
     cd $(mktemp -d)
     trap "cd '$oldpwd'; rm -rf $(pwd)" ERR EXIT
     git clone "https://github.com/neovim/neovim.git"
     cd neovim
-    git co stable
-    make -j CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/home/jan/.local/"
+    # git co stable
+    # CMAKE_BUILD_TYPE=RelWithDebInfo
+    make -j \
+        CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/home/jan/.local/"  \
+        DCMAKE_BUILD_TYPE=Release
     make install
     cd "$oldpwd"
 }
