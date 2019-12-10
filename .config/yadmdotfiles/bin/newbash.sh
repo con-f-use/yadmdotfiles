de1fdb8def32a36ba2de47a2849f0792d806cf42
Jan Christoph Bischko <jbischko@barracuda.com>
2019-12-10 11:43:54 +0100

    newbash template changes

--- .config/yadmdotfiles/bin/newbash.sh
+++ .config/yadmdotfiles/bin/newbash.sh
@@ -9,9 +9,8 @@ author: confus <con-f-use@gmx.net>"
 default_filename=/dev/stdout
 
 libfile="$HOME/.config/yadmdotfiles/bash/jcgb.bash"
-[ -r "$libfile" ] &&
-    source "$libfile" ||
-    { 2>echo "Requires '$libfile'!"; exit 1; }
+[ -r "$libfile" ] || { 2>echo "Requires '$libfile'!"; exit 1; }
+source "$libfile" 
 
 main() {
 filename=${1:-$default_filename}
@@ -19,21 +18,30 @@ cat - > "$filename" <<- EndHereDoc
 #!/bin/bash
 # encoding: UTF-8, break: linux, indent: 4 spaces, lang: bash 3+/eng
 description="./\$0 -h | [args...]
-Here goes a nice description
+<++ Here goes a nice description>
 
 date: $(LANG=en_US date)
 author: ${FULL_NAME:-${USER:-}} <${USER_EMAIL:-a.b@c.de}>"
 
 libfile="\$HOME/.config/yadmdotfiles/bash/jcgb.bash"
-[ -r "\$libfile" ] &&
-    source "\$libfile" ||
-    { 2>echo "Requires '\$libfile'!"; exit 1; }
+[ -r "\$libfile" ] || { 2>echo "Requires '\$libfile'!"; exit 1; }
+source "\$libfile"
 
 main() {
-    find "\$@" -maxdepth 2 -iname "*practice*.md" -print0 2>/dev/null |
-        while IFS= read -r -d \$'\0' itm; do
-            echo "I found: '\$itm'"
-        done
+    <++>
+    # find "\$@" -maxdepth 2 -iname "*practice*.md" -print0 2>/dev/null |
+    #     while IFS= read -r -d \$'\0' itm; do
+    #         echo "I found: '\$itm'"
+    #     done
+
+    # Print only lines where a substitution was made with sed:
+    #sed --regexp-extended --quite 's/bla(.{1,3})blubb)/\1/gp' some_file
+    # Make sustition only in lines that match another pattern: 
+    # sed --regexp-extended '/rs(yn)c/s/<command>/1woo/g' newbash.sh
+
+    # Common rsync options:
+    #rsync --rsh=<command> --partial --append-verify --recursive \
+    #  --human-readable --info=progress2 --compress source destination
 }
 
 if [ "\$0" = "\$BASH_SOURCE" ]; then
@@ -51,3 +59,4 @@ if [ "$0" = "$BASH_SOURCE" ]; then
     usage "$@"
     main "$@"
 fi
+
