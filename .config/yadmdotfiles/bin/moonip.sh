066e504fd1d487c399d21c1afb9f318cb3badd27
con-f-use <con-f-use@gmx.net>
2020-04-10 01:47:56 +0200

    pythonpath and inetip

--- .config/yadmdotfiles/bin/moonip.sh
+++ .config/yadmdotfiles/bin/moonip.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Print ip of moonsoft to stdout and clipboard.
 
+# moonip=$(
+#     ssh cudaws ping moon.emea.cuda-inc.com -c 1 2>/dev/null | 
+#         grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' )
+
 moonip=$(
-    ssh cudaws ping moon.emea.cuda-inc.com -c 1 2>/dev/null | 
-        grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' )
+    ssh jan@10.17.70.10 ping moon.emea.cuda-inc.com -c 1 2>/dev/null ||
+        ssh cudaws ping moon.emea.cuda-inc.com -c 1 2>/dev/null | 
+        grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' ) || exit 1
+)
 
 echo "$moonip"
 #echo "$moonip" |
