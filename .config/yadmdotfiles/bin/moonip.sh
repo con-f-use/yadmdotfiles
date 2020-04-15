06e8edf897576bee8dc6cc603d5cdddae3831c4c
con-f-use <con-f-use@gmx.net>
2020-04-15 15:48:27 +0200

    fix moonip

--- .config/yadmdotfiles/bin/moonip.sh
+++ .config/yadmdotfiles/bin/moonip.sh
@@ -6,9 +6,10 @@
 #         grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' )
 
 moonip=$(
-    ssh jan@10.17.70.10 ping moon.emea.cuda-inc.com -c 1 2>/dev/null ||
+    ssh jan@10.17.70.10 ping moon.emea.cuda-inc.com -c 1 2>/dev/null |
+        grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' || 
         ssh cudaws ping moon.emea.cuda-inc.com -c 1 2>/dev/null | 
-        grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' ) || exit 1
+        grep -Po -m1 '\d{1,3}(\.\d{1,3}){3,3}' || exit 1
 )
 
 echo "$moonip"
