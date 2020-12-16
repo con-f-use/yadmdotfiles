f2580c23c7c3aea5d40cc22dee5562931eec2946
con-f-use <con-f-use@gmx.net>
2020-12-16 12:13:14 +0100

    random episode umpv

--- .config/yadmdotfiles/bin/random-episode.sh
+++ .config/yadmdotfiles/bin/random-episode.sh
@@ -20,13 +20,22 @@ esac
 
 echo ""
 
+if command -v umpv 2>/dev/null; then
+play() {
+    umpv "$@"
+}
+elif command -v vlc 2>/dev/null; then
+play() {
+    vlc --one-instance --playlist-enqueu "$@"
+}
+fi
+
 find "$@" -type f -iregex '.*\(avi\|mp4\|mkv\|m4v\|mpg\|mpeg\|mpeg2\|mpeg4\|ogm\|ogg\|ogx\|ogv\|h264\|264\|m2p\|m4v\|xvid\|h265\|265\|hdmov\|mp2v\|hvec\|webm\|vid\|avc\|exo\|tts\|wmv\|wm\|xmwv\)$' -print0 |
     sort -Rz | #cut -d $'\0' -f-$last |
     while IFS= read -r -d $'\0' file; do
         file=$(readlink -f "$file")
         echo "$i: $file"
-        #/nix/store/xgr6m2f4rpgjnb8lymxrlvqvcwmmq6rv-vlc-3.0.11.1/bin/vlc --one-instance --playlist-enqueu "$file" &
-        umpv "$file" &
+        play "$file" &
         if (( i==0 )) || (( i==-2 )); then sleep 2; (( i++ )); fi
         if (( i>0 )); then (( i++ )); fi
         if (( i>last )); then break; fi
