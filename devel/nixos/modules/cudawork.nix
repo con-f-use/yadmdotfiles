1a9623029ce1f02487cc236707db5642126f3ef3
Jan Christoph Bischko <jbischko@barracuda.com>
2023-09-07 14:03:16 +0200

    cuda: add jupiter jenkins & troubadix

--- devel/nixos/modules/cudawork.nix
+++ devel/nixos/modules/cudawork.nix
@@ -145,6 +145,7 @@ in
         "10.17.50.250" = [ "portal.qa.ngdev.eu.ad.cuda-inc.com" "portal.qa" ];
         "10.17.50.102" = [ "portal-staging.qa.ngdev.eu.ad.cuda-inc.com" "portal-staging.qa" ];
         "10.17.50.246" = [ "jenkins-cgf.qa.ngdev.eu.ad.cuda-inc.com" "jenkins2.qa" ];
+        "10.17.210.76" = [ "jenkins-jupiter.ngdev.eu.ad.cuda-inc.com" ];
         "10.17.6.61" = [ "nixbld01.qa.ngdev.eu.ad.cuda-inc.com" "nixbld01.qa" ];
         "10.17.6.62" = [ "nixbld02.qa.ngdev.eu.ad.cuda-inc.com" "nixbld02.qa" ];
         "10.17.6.63" = [ "nixbld03.qa.ngdev.eu.ad.cuda-inc.com" "nixbld03.qa" ];
@@ -155,6 +156,7 @@ in
         "10.17.210.145" = [ "folsom.ngdev.eu.ad.cuda-inc.com" "folsom.qa" ];
         "10.14.0.22" = [ "docker-c7.3sp.co.uk" ];
         "10.17.33.51" = [ "idefix.ngdev.eu.ad.cuda-inc.com" ];
+        "10.17.33.215" = [ "troubadix.ngdev.eu.ad.cuda-inc.com" ];
         "10.17.210.200" = [ "order.ngdev.eu.ad.cuda-inc.com" ];
       };
 
