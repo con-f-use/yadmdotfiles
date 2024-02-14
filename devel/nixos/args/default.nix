6cfc3d989477f4f614d10d1412227c84fa15c3b1
con-f-use <con-f-use@gmx.net>
2024-02-14 19:02:42 +0100

    git-config: sshsinging, includeif

--- devel/nixos/args/default.nix
+++ devel/nixos/args/default.nix
@@ -1,7 +1,7 @@
 inputs:
 let
   lib = inputs.nixunstable.lib;
-  forSystems = inputs.nixunstable.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+  forSystems = lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
   legacy = forSystems (system: inputs.nixunstable.legacyPackages.${system});
   forPkgs = path: forSystems (system:
     import path { pkgs = legacy.${system}; }
