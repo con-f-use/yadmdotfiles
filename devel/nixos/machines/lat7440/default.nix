77ae78f65ba0259c6a8552701a5abd694c490550
con-f-use <con-f-use@gmx.net>
2025-04-02 13:27:04 +0200

    bcvpn: HOME; worklap: sox

--- devel/nixos/machines/lat7440/default.nix
+++ devel/nixos/machines/lat7440/default.nix
@@ -56,6 +56,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    sox
     curl
     firefox
     signal-desktop
