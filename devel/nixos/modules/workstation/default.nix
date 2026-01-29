d5e62198f1c8f78facb39a9eea9674393fc80900
con-f-use <con-f-use@gmx.net>
2026-01-29 13:40:35 +0100

    workstation: wlcopy

--- devel/nixos/modules/workstation/default.nix
+++ devel/nixos/modules/workstation/default.nix
@@ -1,7 +1,8 @@
-{ config
-, lib
-, pkgs
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 let
   kitty-xterm-link = pkgs.writeScriptBin "xterm" "${pkgs.kitty}/bin/kitty \"$@\"";
@@ -10,66 +11,64 @@ in
   options.roles.workstation = {
     enable = lib.mkEnableOption "Common config for my workstations";
   };
-  config = lib.mkIf config.roles.workstation.enable
-    {
+  config = lib.mkIf config.roles.workstation.enable {
 
-      # programs.mtr.enable = true;
+    # programs.mtr.enable = true;
 
-      programs.gnupg.agent = {
-        enable = true;
-        enableSSHSupport = true;
-        # pinentryFlavor = "gtk2"; # "curses" "tty" "gnome3" "qt"
-        pinentryPackage = pkgs.pinentry-gtk2; # new from 30.03.2024 on
-      };
-      #services.dbus.packages = [ pkgs.gcr ];
-      #services.pcscd.enable = true;
-      # pkill -f gpg-agent; pkill -f pinentry
-      # systemctl --user restart gpg-agent.socket gpg-agent-extra.socket gpg-agent-ssh.socket
-      # systemctl --user restart gpg-agent
-      # gpgconf --reload gpg-agent
-
-      programs.xonsh = {
-        enable = true;
-        #config = '' '';
-      };
-
-      environment.systemPackages = with pkgs;
-      [
-          conservetool
-
-          # Workstation
-          yadm
-          gopass
-          gopass-jsonapi
-          pinentry
-          pinentry-curses
-          pinentry-gtk2
-          mkpasswd
-          zip
-          trash-cli
-          st
-          kitty
-          oathToolkit
-          qrencode
-          ripgrep
-          usbutils
-          lshw
-          lua
-          zoxide
-
-          # Network
-          inetutils
-          dnsutils
-          nmap
-          mtr
-          openssl
-          sshpass
-          nload
-
-          # Multimedia
-          ncmpcpp
-          ffmpeg-full
-          deluge
-        ];
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      # pinentryFlavor = "gtk2"; # "curses" "tty" "gnome3" "qt"
+      pinentryPackage = pkgs.pinentry-gtk2; # new from 30.03.2024 on
     };
+    #services.dbus.packages = [ pkgs.gcr ];
+    #services.pcscd.enable = true;
+    # pkill -f gpg-agent; pkill -f pinentry
+    # systemctl --user restart gpg-agent.socket gpg-agent-extra.socket gpg-agent-ssh.socket
+    # systemctl --user restart gpg-agent
+    # gpgconf --reload gpg-agent
+
+    programs.xonsh = {
+      enable = true;
+      #config = '' '';
+    };
+
+    environment.systemPackages = with pkgs; [
+      conservetool
+
+      # Workstation
+      yadm
+      gopass
+      gopass-jsonapi
+      pinentry-curses
+      pinentry-gtk2
+      mkpasswd
+      zip
+      trash-cli
+      st
+      kitty
+      oath-toolkit
+      qrencode
+      ripgrep
+      usbutils
+      lshw
+      lua
+      wl-clipboard-rs
+      zoxide
+
+      # Network
+      inetutils
+      dnsutils
+      nmap
+      mtr
+      openssl
+      sshpass
+      nload
+
+      # Multimedia
+      ncmpcpp
+      ffmpeg-full
+      deluge
+    ];
+  };
 }
